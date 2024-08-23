2f65216cb49216d4c20939d5948adf9faa4a77e2
Steven <146814279+Zephyrine16@users.noreply.github.com>
2024-08-23 20:48:34 +0800

    Add files via upload

---
+++ ComputeAverageApp.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+class ComputeAverageProgram
+{
+    static void Main()
+    {
+        double Sum_Of_Grades = 0;
+        
+        //Info gathering
+        Console.Write("Enter your name: ");
+        string name = Console.ReadLine();
+        
+        Console.Write("Enter your program: ");
+        string program = Console.ReadLine();
+
+        Console.Write("Total number of subjects: ");
+        int subjects = Convert.ToInt32(Console.ReadLine());
+        
+        double[] grades = new double[subjects];
+        int Subject_Number = 0; //The starting point is at 0
+        do
+        {
+            Console.Write($"Enter your grade in subject {Subject_Number + 1}: ");
+            string grade = Console.ReadLine();
+
+            if (Double.TryParse(grade, out grades[0]))
+            {
+                grades[Subject_Number] = Convert.ToDouble(grade);  //Store the grade in an array
+                Sum_Of_Grades += grades[0];  //Add the grade to the sum
+                Subject_Number++;  //Add a number every loop
+            }
+            else
+            {
+                Console.WriteLine("Invalid! Please enter a valid grade.");
+            }
+        } while (Subject_Number < subjects);
+        
+        Console.Write($"\nName: {name}\n");
+        Console.WriteLine($"Program: {program}");
+        Console.WriteLine($"Subjects: {subjects}\n");
+        
+        //Calculate the average grade
+        double average = Sum_Of_Grades / subjects;
+        double Rounded_Average = Math.Round(average);  //Round the average
+        int Rounded_Average_Int = (int)Rounded_Average;
+        Console.Write($"Your average grade is: {average}\n");
+        Console.Write($"The rounded off average is {Rounded_Average_Int}\n");
+        
+        Console.Write("\nPress any key to exit...");
+        Console.ReadKey();
+    }
+}
---
+++ DataTypesApp.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+
+class DataTypesApp
+{
+    static void Main()
+    {
+        //Dictionary for apple tyeps and their prices
+        Dictionary<string, double> Items = new Dictionary<string, double>()
+        {
+            {"Gravenstein", 0.55},
+            {"Antonovka", 0.43},
+            {"Court Pendu Plat", 0.47}, 
+            {"McIntosh", 0.39},
+            {"Egremont Russet", 0.57},
+            {"Red Falstaff", 0.38}
+        };
+
+        double Total_Cost = 0.0;  //Variable used for tracking of the total cost of apples
+        bool shop = true; //Boolean to control the loop
+        
+        Console.WriteLine("Welcome to Apple Orchard! We're thrilled to offer you our finest apples, picked at the" +
+                      " peak of perfection just for you.");
+        Console.WriteLine("Check out our prices!\n");
+
+        //Main loop
+        while (shop)
+        {
+          // Display of available apples
+            foreach (var item in Items)
+            {
+                Console.WriteLine($"{item.Key}: £{item.Value}");
+            }
+            Console.Write("Enter the type of apple you want: ");
+            string apple = Console.ReadLine();
+            
+            if (Items.ContainsKey(apple)) //Check if the apple input exists
+            {
+                Total_Cost += Items[apple];  //Add the price to the total cost
+                Console.WriteLine($"{apple} has been added to the total.");
+            }
+            else
+            {
+                Console.WriteLine("Apple doesn't exist!");  //Tell the user if the apple type doesn't exist
+            }
+            Console.Write("\nDo you want another apple? (y/n): ");
+            string answer = Console.ReadLine();
+
+            if (answer.Equals("yes", StringComparison.OrdinalIgnoreCase)) //Check the user's response and decide whether to continue or not
+            {
+                shop = true;
+            }
+            else if (answer.Equals("no", StringComparison.OrdinalIgnoreCase))
+            {
+                shop = false;
+            }
+        }
+        Console.WriteLine($"Your total cost is £{Total_Cost:F2}");  //Display of final total cost
+        int Converted_Total_Cost = (int)Total_Cost;  //Convert the total cost to int
+        int Converted_Total_Cost_Rounded = (int)Math.Round(Total_Cost);
+        Console.WriteLine($"The converted total cost is: £{Converted_Total_Cost_Rounded}");
+        Console.WriteLine("\nPress any key to exit...");
+        Console.ReadKey();
+    }
+}

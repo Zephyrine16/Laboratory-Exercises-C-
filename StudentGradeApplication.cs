2a59fa1e84d0c4cc5512c5975bcc6dae27c061cc
Steven <146814279+Zephyrine16@users.noreply.github.com>
2024-08-27 20:18:56 +0800

    Add files via upload

---
+++ StudentGradeApplication.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+class frmStudentGradeProgram
+{
+    static void Main(String [] args)
+    {
+        Console.Write("Name: ");
+        string name = Console.ReadLine();
+        Console.WriteLine("Grades:\n");
+        
+        Console.Write("English: ");
+        double english = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Math: ");
+        double math = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Science: ");
+        double science = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Filipino: ");
+        double filipino = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("History: ");
+        double history = Convert.ToDouble(Console.ReadLine());
+
+
+        double Sum = english + math + science + filipino + history;
+        double Average = Sum / 5;
+
+        if(Average >= 75.00)
+        {
+            Console.Write("\nThe student passed.");
+            Console.Write($"\nThe general average of {name} is {Average:F2}.\n");
+        }
+        else if(Average < 75.00)
+        {
+            Console.Write("\nThe student failed.");
+            Console.Write($"The general average of {name} is {Average:F2}.\n");
+        }
+    }
+}
